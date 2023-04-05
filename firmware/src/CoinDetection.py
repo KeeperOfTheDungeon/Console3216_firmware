f420ab4d41d0e226e766689131de88da41ee3185
Serg86 <12775401+Serg86@users.noreply.github.com>
2023-04-05 11:51:01 +0200

    - Bezeichnungen an Python Konvention angepasst (_ = Protected; __ = Private) - import Dateiendung entfernt - ctypes anstelle einfachem int verwendet

--- firmware/src/CoinDetection.py
+++ firmware/src/CoinDetection.py
@@ -1,23 +1,26 @@
+from ctypes import c_ubyte
+
 import RPi.GPIO as GPIO
 # TODO
-import RgbLed.py
+import RgbLed
 
 COIN_PIN = 19
 GAME_COST = 1
 
 class CoinDetection:
-    _balance: int
+    # TODO
+    _balance: c_ubyte
     # TODO
     _rgbLed: RgbLed
 
-    def _setLed(self):
+    def __setLed(self):
         if (self._balance >= GAME_COST):
             self._rgbLed.setLEDColor(0,3,0)
         else:
             self._rgbLed.setLEDColor(3,0,0)
         pass
 
-    def _detectFallingEdge(self):
+    def __detectFallingEdge(self):
         self._balance += 1
         self._setLed()
         pass
