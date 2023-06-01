dc65918ad42f222fc712b3f368678f8dfae4e588
Serg86 <129606991+serg-hka@users.noreply.github.com>
2023-06-01 21:49:47 +0200

    Klassenrahmen erstellt

---
+++ firmware/src/Sine.py
@@ -0,0 +1,12 @@
+
+class Sine:
+    def __init__(self) -> None:
+        pass
+
+    @classmethod
+    def getSineValue(cls, angle: int) -> int:
+        pass
+
+    @classmethod
+    def getCosineValue(cls, angle: int) -> int:
+        pass
