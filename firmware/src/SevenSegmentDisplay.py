a314c85cbd63555eac4f59c38cfa2357c78524b1
Serg86 <129606991+serg-hka@users.noreply.github.com>
2023-06-01 21:49:36 +0200

    Klassenrahmen erstellt

---
+++ firmware/src/SevenSegmentDisplay.py
@@ -0,0 +1,22 @@
+
+from Ht16k33 import *
+
+# TODO C++ Klassendeklaration:
+# class SevenSegmentDisplay	: public Ht16k33 { ... };
+class SevenSegmentDisplay(Ht16k33):
+    def setNumberAt(self, number: int, position: int):
+        pass
+    def setSegmentsAt(self, segments: int, position: int):
+        pass
+    def clearDotAt(self, number: int, position: int):
+        pass
+    def setDotAt(self, number: int, position: int):
+        pass
+    def setColon(self):
+        pass
+    def clearColon(self):
+        pass
+    def toggleColon(self):
+        pass
+    def setNumber(self, value: int):
+        pass
