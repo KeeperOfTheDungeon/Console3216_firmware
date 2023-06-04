41b24c45737127b1661a1070a7f4c32c23131763
Serg86 <129606991+serg-hka@users.noreply.github.com>
2023-06-04 23:30:13 +0200

    - byteConverter aus der Klasse geschoben - Alle Methoden implementiert

--- firmware/src/MIDI_Control_Commands.py
+++ firmware/src/MIDI_Control_Commands.py
@@ -2,9 +2,27 @@
 # TODO Nicht sicher, ob es auch ohne die C Union geht
 from ctypes import Union, c_int16, c_int8, Structure
 
+
 class halfValue(Structure):
         _fields_ = [("secondByte", c_int8),
                     ("firstByte", c_int8)]
+
+# TODO Eventuell kommen wir auch ohne aus
+# C++ Code:
+# union byteConverter {
+#     uint16_t value;
+#     struct {
+#         int8_t secondByte;
+#         int8_t firstByte;
+#     };
+# };
+
+# TODO Muss getestet werden
+class byteConverter(Union):
+    _anonymous_ = ("h",)
+    _fields_ = [("value", c_int16),
+                ("h", halfValue)]
+
 
 class MIDI_Control_Commands:
     __SERIAL_BAUD = 74880
@@ -15,49 +33,69 @@ class MIDI_Control_Commands:
     __COMMAND_CODE_RESUME_TRACK = 0x05
     __COMMAND_CODE_RESTART_TRACK = 0x06
 
-    # TODO Eventuell kommen wir auch ohne aus
-    # C++ Code:
-    # union byteConverter {
-    #     uint16_t value;
-    #     struct {
-    #         int8_t secondByte;
-    #         int8_t firstByte;
-    #     };
-    # };
-
-    # TODO Muss getestet werden
-    class byteConverter(Union):
-        _anonymous_ = ("h",)
-        _fields_ = [("value", c_int16),
-                    ("h", halfValue)]
-
     def __init__(self) -> None:
         pass
 
+    # Hilfsmethode
     @classmethod
-    def playTrack(cls, trackId: int, tempo: int):
-        pass
+    def serialWriteData(cls, command: int, trackId: int, tempo: int = -1):
+        byteConverter: byteConverter
+        byteConverter.value = trackId
+
+        firstByteTrackId = byteConverter.firstByte
+        secondByteTrackId = byteConverter.secondByte
+
+        if tempo != -1:
+            byteConverter.value = tempo
+            firstByteTempo = byteConverter.firstByte
+            secondByteTempo = byteConverter.secondByte
+
+            data = [
+                command,
+                firstByteTrackId,
+                secondByteTrackId,
+                firstByteTempo,
+                secondByteTempo
+            ]
+        else:
+            data = [
+                command,
+                firstByteTrackId,
+                secondByteTrackId
+            ]
+        
+        # TODO C++ Quellcode:
+        # MIDI_SERIAL.write(data, 5);
+        # MIDI_SERIAL ist Serial aus Arduino Code
+
+        return 0
 
     @classmethod
-    def changeTempo(cls, trackId: int, tempo: int):
-        pass
+    def playTrack(cls, trackId: int, tempo: int) -> int:
+        return cls.serialWriteData(cls.__COMMAND_CODE_PLAY_TRACK, trackId, tempo)
 
     @classmethod
-    def pauseTrack(cls, trackId: int):
-        pass
+    def changeTempo(cls, trackId: int, tempo: int) -> int:
+        return cls.serialWriteData(cls.__COMMAND_CODE_CHANGE_TEMPO, trackId, tempo)
 
     @classmethod
-    def stopTrack(cls, trackId: int):
-        pass
+    def pauseTrack(cls, trackId: int) -> int:
+        return cls.serialWriteData(cls.__COMMAND_CODE_PAUSE_TRACK, trackId)
 
     @classmethod
-    def resumeTrack(cls, trackId: int):
-        pass
+    def stopTrack(cls, trackId: int) -> int:
+        return cls.serialWriteData(cls.__COMMAND_CODE_STOP_TRACK, trackId)
 
     @classmethod
-    def restartTrack(cls, trackId: int):
-        pass
+    def resumeTrack(cls, trackId: int) -> int:
+        return cls.serialWriteData(cls.__COMMAND_CODE_RESUME_TRACK, trackId)
+
+    @classmethod
+    def restartTrack(cls, trackId: int) -> int:
+        return cls.serialWriteData(cls.__COMMAND_CODE_RESTART_TRACK, trackId)
 
     @classmethod
     def setupMidi(cls):
+        # TODO C++ Quellcode:
+        # MIDI_SERIAL.begin(MIDI_Control_Commands::SERIAL_BAUD);
         pass
