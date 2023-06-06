5327620704666919e55d3ec77e9616e1cc23e52d
Serg86 <129606991+serg-hka@users.noreply.github.com>
2023-06-06 12:52:42 +0200

    setPosition() und move() implementiert

--- firmware/src/Sprite.py
+++ firmware/src/Sprite.py
@@ -3,18 +3,26 @@ from Display import *
 
 class Sprite:
     def __init__(self, xPos: int, yPos: int, xExtend: int, yExtend: int) -> None:
-        self._xPos: int
-        self._yPos: int
-        self._xExtend: int
-        self._yExtend: int
-        self._bitmap: int
+        self._xPos: int = xPos
+        self._yPos: int = yPos
+        self._xExtend: int = xExtend
+        self._yExtend: int = yExtend
+        # C++ Quellcode:
+        # this->bitmap = new uint16_t [yExtend * xExtend];
+        self._bitmap = [0] * (xExtend * yExtend)
         self._direction: int
         self._active: bool
 
     def setPosition(self, newXPos: int, newYPos: int):
+        self._xPos = newXPos
+        self._yPos = newYPos
         pass
+
     def move(self, xDelta: int, yDelta: int):
+        self._xPos += xDelta
+        self._yPos += yDelta
         pass
+
     def getXPos(self) -> int:
         pass
     def getYPos(self) -> int:
