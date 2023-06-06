18bc73f0697d4d882dc7f9a32f4c42192c97488a
Serg86 <129606991+serg-hka@users.noreply.github.com>
2023-06-06 10:01:24 +0200

    numbertable Liste hinzugefügt

--- firmware/src/SevenSegmentDisplay.py
+++ firmware/src/SevenSegmentDisplay.py
@@ -1,6 +1,27 @@
 
 from Ht16k33 import *
 
+# TODO In C++ Quellcode eine Konstante:
+# static const uint8_t numbertable[] = { ... }
+numbertable = [
+    0x3F,  # 0
+    0x06,  # 1
+    0x5B,  # 2
+    0x4F,  # 3
+    0x66,  # 4
+    0x6D,  # 5
+    0x7D,  # 6
+    0x07,  # 7
+    0x7F,  # 8
+    0x6F,  # 9
+    0x77,  # a
+    0x7C,  # b
+    0x39,  # C
+    0x5E,  # d
+    0x79,  # E
+    0x71,  # F
+]
+
 # TODO C++ Klassendeklaration:
 # class SevenSegmentDisplay	: public Ht16k33 { ... };
 class SevenSegmentDisplay(Ht16k33):
