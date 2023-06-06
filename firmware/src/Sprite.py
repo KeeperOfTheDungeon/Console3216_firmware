e26bfd1dd05f16540b2ce15d14d10f1173a4c147
Serg86 <129606991+serg-hka@users.noreply.github.com>
2023-06-06 13:06:31 +0200

    draw() implementiert

--- firmware/src/Sprite.py
+++ firmware/src/Sprite.py
@@ -1,6 +1,17 @@
 
 from Display import *
 
+# TODO Die Farben werden jetzt mit 3 ints (RGB) angegeben
+# Die Liste _bitmap ist in C++ ein Array aus ints, die
+# auf jeder Position einen Farbcode beinhalten.
+# In Dieser Implementierung wird ein neues Color Objekt
+# an jeder Position angelegt
+class Color:
+    def __init__(self, r, g, b):
+        self.r = r
+        self.g = g
+        self.b = b
+
 class Sprite:
     def __init__(self, xPos: int, yPos: int, xExtend: int, yExtend: int) -> None:
         self._xPos: int = xPos
@@ -9,7 +20,7 @@ class Sprite:
         self._yExtend: int = yExtend
         # C++ Quellcode:
         # this->bitmap = new uint16_t [yExtend * xExtend];
-        self._bitmap = [0] * (xExtend * yExtend)
+        self._bitmap = [Color(0, 0, 0)] * (xExtend * yExtend)
         self._direction: int
         self._active: bool
 
@@ -40,7 +51,15 @@ class Sprite:
         self._active = False
         pass
 
+    # TODO Farbe wird nicht mehr mit nur einem int angegeben
     def draw(self):
+        if self._active:
+            i = 0
+
+            for x in range(self._xPos, self._xExtend + self._xPos):
+                for y in range(self._yPos, self._yExtend + self._yPos):
+                    Display.drawPixel(x, y, self._bitmap[i].r, self._bitmap[i].g, self._bitmap[i].b)
+                    i += 1
         pass
 
     def mirrorY(self):
